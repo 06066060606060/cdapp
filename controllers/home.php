e98a7ffabb58411ce656be22c78b1007dca81ca4
Joaquim410 <j.willsmith974@gmail.com>
2022-10-28 09:41:44 +0400

    login

--- controllers/home.php
+++ controllers/home.php
@@ -18,23 +18,34 @@ date_default_timezone_set('Europe/Paris');
 </head>
 
 <body>
-      <header>
-      
-      </header>
+  <header>
 
-      <div>
-    <h1>Login</h1>
-    <form action="/controllers/authenticate.php" method="post">
-      <label for="username">
-      </label>
-      <input type="text" name="username" placeholder="Username" id="username" required>
-      <label for="password">
-      </label>
-      <input type="password" name="password" placeholder="Password" id="password" required>
-      <input type="submit" value="Login">
-    </form>
-  </div>
-    
+  </header>
+
+  <div class="bg-gray-700">
+    <div class="flex min-h-screen items-center justify-center">
+
+
+      <div class="min-h-1/2 bg-gray-900  border border-gray-900 rounded-2xl">
+      <form action="./controllers/authenticate.php" method="post">
+        <div class="mx-4 sm:mx-24 md:mx-34 lg:mx-56 mx-auto  flex items-center space-y-4 py-16 font-semibold text-gray-500 flex-col">
+
+          <h1 class="text-white text-2xl">Login</h1>
+     
+
+          <input class="w-full p-2 bg-gray-900 rounded-md  border border-gray-700 focus:border-blue-700" type="text" name="username" placeholder="Username" id="username" required>
+
+          <input class="w-full p-2 bg-gray-900 rounded-md border border-gray-700 " type="password" name="password" placeholder="Password" id="password" required>
+
+          <input type="submit" class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700" value="Login">
+      
+        </div>
+        </form>
+
+      </div>
+
+    </div>
+
 </body>
 
 </html>
