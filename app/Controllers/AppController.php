18a4b24fff0355dabf1b25c8292307a985e04dc4
06066060606060 <xmicky+git@hotmail.fr>
2022-11-03 10:16:22 +0400

    ajout graph

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -11,63 +11,33 @@ use database\DBConnection;
 
 class AppController extends Controller {
 
-
-
-
    public function loginPost() {
       
-      $this->isAdmin();
-
-      $user = (new User($this->getDB()))->getByEmail($_POST['email']);
-          
-      if (password_verify($_POST['password'], $user->password )) {
-
+     $this->isAdmin();
+     $user = (new User($this->getDB()))->getByEmail($_POST['email']);
+     if (password_verify($_POST['password'], $user->password )) {
      $_SESSION['auth'] = $user->admin;
      $_SESSION['id'] = $user->id;
      return header('location: /dashboard?success=true');
-
       } else {
-
          return header('location: /');
       }
-      
       }
       
    
       public function logout()
       {
-     
         session_destroy();
-       
         header('Location: /');
       }
 
+  //full api
 
-    public function dashboard()
-    {
-      $this->isAdmin();
-       return $this->view('app.dashboard');
-    }
-
-    public function calendar()
-    {
-      $this->isAdmin();
-       return $this->view('app.calendar');
-    }
-
-    public function history()
-    {
-      $this->isAdmin();
-       return $this->view('app.history');
-    }
-
-  
    public function getApi(){
-
-         $con = mysqli_connect("localhost", "root", "", "webapp");
+     $con = mysqli_connect("localhost", "root", "", "webapp");
      $response = array();
      if($con){
-         $sql = "SELECT * FROM user_data ORDER BY date DESC ";
+         $sql = "SELECT * FROM user_data WHERE user_data.user_id = '1' ORDER BY date DESC";
          //  WHERE user_id = {$_SESSION['id']}";
          $result = mysqli_query($con, $sql);
          if($result){
@@ -85,8 +55,9 @@ class AppController extends Controller {
      }else{
          echo "Database connection failed";
      }
-      }
+ }
 
+ //day api
       public function getApiToDay(){
 
          $con = mysqli_connect("localhost", "root", "", "webapp");
@@ -114,4 +85,66 @@ class AppController extends Controller {
      }
       }
    
+
+      public function getApiWeek(){
+
+        $con = mysqli_connect("localhost", "root", "", "webapp");
+    $response = array();
+    $today = date("Y-m-d");
+
+    if($con){
+        $sql = "SELECT * FROM user_data WHERE user_data.date = '$today'" ;
+        // WHERE user_id = {$_SESSION['id']}";
+        $result = mysqli_query($con, $sql);
+        if($result){
+            $x = 0;
+            while($row = mysqli_fetch_assoc($result)){
+                $response[$x]['id'] = $row['id'];
+                $response[$x]['user_id'] = $row['user_id'];
+                $response[$x]['h_rate'] = $row['h_rate'];
+                $response[$x]['work_time'] = $row['work_time'];
+                $response[$x]['date']= $row['date'];       
+                $x++;
+            }
+    echo json_encode($response, JSON_PRETTY_PRINT);
+            }
+    }else{
+        echo "Database connection failed";
+    }
+     }
+  
+     public function getApiMonth(){
+
+        $con = mysqli_connect("localhost", "root", "", "webapp");
+    $response = array();
+    $today = date("Y-m-d");
+
+    if($con){
+        $sql = "SELECT * FROM user_data WHERE user_data.date = '$today'" ;
+        // WHERE user_id = {$_SESSION['id']}";
+        $result = mysqli_query($con, $sql);
+        if($result){
+            $x = 0;
+            while($row = mysqli_fetch_assoc($result)){
+                $response[$x]['id'] = $row['id'];
+                $response[$x]['user_id'] = $row['user_id'];
+                $response[$x]['h_rate'] = $row['h_rate'];
+                $response[$x]['work_time'] = $row['work_time'];
+                $response[$x]['date']= $row['date'];       
+                $x++;
+            }
+    echo json_encode($response, JSON_PRETTY_PRINT);
+            }
+    }else{
+        echo "Database connection failed";
+    }
+     }
+
+
+
+
+
+
+
+
    }
